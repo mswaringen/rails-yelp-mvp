1e6b3183d7a3b38d2a14261d1c59a3b81c30ba58
mark swaringen <mswaringen@gmail.com>
2019-05-15 16:18:03 +0100

    all done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying database..."
+Restaurant.destroy_all
 
-# Restaurant.destroy_all
+puts "Seeding database..."
 
-# Restaurant.new()
+rest_attr = [{
+  name: 'DimSumYum',
+  address: '45 8th st',
+  category: 'chinese'
+},
+{
+  name: 'PizzaPizza',
+  address: '63 Main st',
+  category: 'italian'
+},
+{
+  name: 'Tamagotchi',
+  address: '119 South Houston st',
+  category: 'japanese'
+},
+{
+  name: 'Crepes4All',
+  address: '99 12th ave',
+  category: 'french'
+},
+{
+  name: 'BeerGardenz',
+  address: '324 Beer blvd',
+  category: 'belgian'
+}]
+
+Restaurant.create!(rest_attr)
+puts "Finished!"
